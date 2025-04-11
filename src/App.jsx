f971f688330b6707a968c9b6cbad78b7e3ab2dbf
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2025-04-10 20:50:40 -0500

    initial pages and components with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,139 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/HomePage.jsx";
+import Asian from "./pages/asian/Asian.jsx";
+import African from "./pages/african/African.jsx";
+import European from "./pages/european/European.jsx";
+import SouthAmerica from "./pages/southamerican/SouthAmerican.jsx";
+import NorthAmerica from "./pages/northamerican/NorthAmerican.jsx"
+import MyKitchen from "./pages/mykitchen/MyKitchen.jsx";
+import Cooked from "./pages/cooked/Cooked.jsx";
+import Favorite from "./pages/favorite/Favorite.jsx";
+import Pantry from "./pages/pantry/Pantry.jsx";
+import Cart from "./pages/cart/Cart.jsx";
+// import ProtectedRoute from "./context/ProtectedRoute.jsx";
+import SamosaRecipe from "./pages/african/Samosas.jsx"
+
 import './App.css'
 
 
-function App() {
-  const [count, setCount] = useState(0)
 
+// import viteLogo from '/vite.svg'
+
+// function App() {
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         </div>
+//     </>
+//   )
+// }
+
+export default function App(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+<BrowserRouter basename="/">
+<Routes>
+    <Route
+      path="/"
+      element={
+        <HomePage/>
+      }
+    />
+
+<Route
+      path="/African"
+      element={
+        <African/>
+      }
+    />
+
+<Route
+      path="/SamosaRecipe"
+      element={
+        <SamosaRecipe/>
+      }
+    />
+
+<Route
+      path="/Asian"
+      element={
+        <Asian/>
+      }
+    />
+
+<Route
+      path="/European"
+      element={
+        <European/>
+      }
+    />
+
+<Route
+      path="/SouthAmerican"
+      element={
+        <SouthAmerica/>
+      }
+    />
+
+<Route
+      path="/NorthAmerican"
+      element={
+        <NorthAmerica/>
+      }
+    />
+
+<Route
+      path="/MyKitchen"
+      element={
+        // <ProtectedRoute>
+          <MyKitchen/>
+        // </ProtectedRoute>
+      }
+    />
+
+<Route
+      path="/Cooked"
+      element={
+        // <ProtectedRoute>
+          <Cooked/>
+        // </ProtectedRoute>
+      }
+    />  
+
+<Route
+      path="/Favorite"
+      element={
+        // <ProtectedRoute>
+          <Favorite/>
+        // </ProtectedRoute>     
+       }
+    />
+
+<Route
+      path="/Pantry"
+      element={
+        // <ProtectedRoute>
+          <Pantry/>
+        // </ProtectedRoute>     
+       }
+    />
+
+<Route
+      path="/Cart"
+      element={
+        // <ProtectedRoute>
+          <Cart/>
+        // </ProtectedRoute>     
+       }
+    />  
+</Routes>
+
+
+
+</BrowserRouter>  )
 }
 
-export default App
