c374031693121e4a3b43abe8a71c7221a3e2c9f2
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2025-06-12 23:40:25 -0500

    switching from trailwind to regular css

--- src/pages/african/African.jsx
+++ src/pages/african/African.jsx
@@ -5,12 +5,8 @@ const African = () => {
   return (
     <div class> 
 
-      <div class = "flex"> 
       <div class = "h-screen bg-[url('/images/scrapbook.jpg')] bg-cover bg-no-repeat bg-center">
-        <div>
-        <Nav/>
-        </div>
-      </div>
+        
       </div>
     </div>
   )
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,7 +1,9 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom"
+import "./HomePage.css"
 import Nav from "../../components/nav/Nav"
 import Cover from "../../assets/images/cover.jpg"
 import Login from "../../components/login/Login.jsx"
+
 
 
 export default function HomePage() {
@@ -10,23 +12,22 @@ export default function HomePage() {
       <div>
         FOODS LAB
         <Link to="/Cart">🛍️</Link> 
+
+        <div className = "nav">
+        <Nav/>
+        </div>
       </div>
 
-      <div class="flex">
-        <div class = "h-screen bg-cover bg-center">
+      <div className="grid" >
+        <div>
           <img
           src={Cover}
           alt="Cookbook"
-          class= "object-cover h-full w-full "
           />
         </div>
 
 
-        <div class = "flex w-1/12 ">
-        <Nav/>
-        </div>
-
-        <div class = "flex-1/4">
+        <div className="login">
         <Login/>
         </div>
 
